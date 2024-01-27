8b6dd285f9ce52b7de2b9e90ea993f7c4a8f4903
Matthew Irizarry <matthewjirizarry@gmail.com>
2024-01-27 15:18:34 -0600

    Fixed the authentication and it actually checks the database for the right password now. At this point, all we need to do is implement route protection.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import Input from "~/components/ui/form/Input";
 import { Fragment } from "react";
 
 export default function Home() {
+
+    
+
     return (
         <Fragment>
             <h1>Style Guide</h1>
