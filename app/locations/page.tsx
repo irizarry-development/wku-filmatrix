3efb081d1b95ba4089031b7f7f6b9ce4269bb470
Matthew Irizarry <matthewjirizarry@gmail.com>
2024-04-06 01:20:37 -0500

    MERGE CONFLICTS: Updated to use the new pagination and searching.

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,90 +1,78 @@
-'use client';
-import Link from "next/link";
-import { FaCirclePlus } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import Table from "~/components/ui/table/Table";
 import TableRow from "~/components/ui/table/TableRow";
 import prisma from "~/lib/prisma";
-import { useState, useEffect } from "react"; 
 
 const headers = ["Name", "Address", "Description", "Phone", "Email", "Contact", "Keywords", ""];
 
-interface Location {
-  id: string;
-  locationName: string;
-  locationAddress: string;
-  locationDescription: string;
-  locationPhone: string;
-  locationEmail: string;
-  locationContactName: string;
-  locationKeywords: string;
+interface LocationDatabaseProps {
+  searchParams: {
+    search: string
+  }
 }
 
-const LocationDatabase = () => {
-    const [locationData, setLocationData] = useState<Location[]>([]);
-    const [searchTerm, setSearchTerm] = useState('');
+export default async function LocationDatabase(
+  {
+    searchParams: {
+      search
+    }
+  }: LocationDatabaseProps
+) {
 
-    useEffect(() => { 
+  let searchData = null;
 
-      const fetchSearchResults = async () => {
-        const response = await fetch(`/api/v1/locations/search?query=${encodeURIComponent(searchTerm)}`);
-        if (!response.ok) {
-          console.error('Failed to fetch:', response.statusText);
-          return;
-        }
-        const data = await response.json();
-        console.log(data);
-        setLocationData(data);
-      };
+  if (!search) {
+    searchData = await prisma.location.findMany()
+  } else {
+    searchData = await prisma.location.findMany({
+      where: {
+        OR: [
+          { locationName: { contains: search, mode: 'insensitive' } },
+          { locationDescription: { contains: search, mode: 'insensitive' } },
+        ],
+      }
+    });
+  }
 
-      fetchSearchResults();
-    }, [searchTerm]); 
-
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchTerm(event.target.value);
-    };
-
-    return (
-      <section className="database-page">
-        <section className="database-page-header">
-          <h1>Locations</h1>
-          <Link href="/locations/add" className="database-page-add">
-            <FaCirclePlus />
-          </Link>
-          <input 
-            type="text"
-            placeholder="Search locations..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-        </section>
-        <section className="database-content">
-          
-        <Table title="Locations" headers={headers}>
-            {
-                (locationData.length > 0) &&
-                locationData.map((loc, i) => (
-                    <TableRow
-                        key={i}
-                        type='Location'
-                        id={loc.id}
-                        name={loc.locationName}
-                        fields={[
-                            loc.locationName,
-                            loc.locationAddress,
-                            loc.locationDescription,
-                            loc.locationPhone,
-                            loc.locationEmail,
-                            loc.locationContactName,
-                            loc.locationKeywords,
-                        ]}
-                        deleteUrl='/api/v1/locations'
-                    />
-                ))
-            }
-        </Table>
-        </section>
+  return (
+    <section className="database-page">
+      <section className="database-page-header">
+        <h1>Locations</h1>
+        <form id="location-search-form" action={`/locations`} method="GET">
+          <label>
+            <input type="text" name="search" placeholder="Search" />
+          </label>
+          <button type="submit">
+            <FaMagnifyingGlass />
+          </button>
+        </form>
       </section>
-    );
-};
+      <section className="database-content">
 
-export default LocationDatabase;
+        <Table title="Locations" headers={headers}>
+          {
+            (searchData.length > 0) &&
+            searchData.map((loc, i) => (
+              <TableRow
+                key={i}
+                type='Locations'
+                id={loc.id}
+                name={loc.locationName}
+                fields={[
+                  loc.locationName,
+                  loc.locationAddress,
+                  loc.locationDescription,
+                  loc.locationPhone,
+                  loc.locationEmail,
+                  loc.locationContactName,
+                  loc.locationKeywords,
+                ]}
+                deleteUrl='/api/v1/locations'
+              />
+            ))
+          }
+        </Table>
+      </section>
+    </section>
+  );
+};
