fa81650d3499d0cf5b095fbcbf0d545157520525
Zachary Vance <zach.a.vance@gmail.com>
2024-04-12 11:24:45 -0500

    - added remaining links

--- app/page.tsx
+++ app/page.tsx
@@ -18,15 +18,49 @@ export default async function Home() {
         { url: "https://www.wkufilm.com/wp-content/uploads/2021/08/Permits.zip", text: "Permits"},
         { url: "https://www.wkufilm.com/wp-content/uploads/2021/09/Pre-Production.zip", text: "Pre-Production"},
         { url: "https://www.wkufilm.com/wp-content/uploads/2021/08/Safety.zip", text: "Safety"},
-
     ];
+    const postproductionLinks = [
+        { url: "https://forms.gle/f54U6t7Csfshjqgs5", text:"Post Hall Reservations"},
+        // { url: "", text: "Soundsnap"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2021/08/VFX-APPROVAL-FORM.pdf", text: "VFX Approval Form"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2024/03/TITLE-CREDIT-PROTOCOL-2024.pdf", text: "End Credit Protocol"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2023/08/AE-Workflows.zip", text: "Assistant Editing Workflow"},
+        { url: "https://www.wkufilm.com/end-credit-elements/", text: "End Credit Elements"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2022/11/TimeCode-Sync-in-Premiere-Pro.pdf", text: "Timecode Sync"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2021/09/Press-Kit-and-Festival-Strategy.zip", text: "Press Kit & Festival Strategy"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2021/08/Dailies-Screening-Notes.xlsx", text: "Dailies Screening Notes"},
+        { url: "https://www.wkufilm.com/wp-content/uploads/2021/08/POSTER-USE.pdf", text: "Poster Use Form"},
+    ];
+    const studentDatabaseLinks = [
+        { url: "https://www.wkufilm.com/location-database/", text: "Location Database"},
+    ]
+    const helpfulLinks = [
+        { url: "http://wku.edu/film", text: "School of Media"},
+        { url: "https://www.wku.edu/studyaway/programs/sa-winter/sundancewinter2020.php", text: "WKU Sundance Study Away"},
+        { url: "http://southernkentuckyfilmcommission.com/", text: "Southern Kentucky Film Commision"},
+        { url: "http://filmoffice.ky.gov/", text: "Kenctuky Film Office"},        
+    ]
+    const jobLinks = [
+        { url: "http://www.mandy.com/index.php?country=US", text: "Mandy.com (Production Jobs)"},
+        { url: "http://staffmeup.com/", text: "Staff Me Up (Production Jobs)"},
+        { url: "http://www.anonymousproductionassistant.com/uta-joblist/", text: "UTA Job List (Industry Jobs, Development, Agencies, etc."},
+    ]
+    const internshipLinks = [
+        { url: "", text: "Television Academy (Emmys) Internship Program"},
+    ]
+
+
     return (
         <Fragment>
             <fieldset className="drawer-fieldset">
-                <legend className="drawer-legend">Helpful Links</legend>
+                <legend className="drawer-legend">Current Student Resources</legend>
                 <Drawer title="WKU Film Student Handbook" links={studentHandbookLinks} />
                 <Drawer title="Production" links={productionLinks} />
-                <Drawer title="Production" links={productionLinks} />
+                <Drawer title="Post-Production" links={postproductionLinks} />
+                <Drawer title="Student Database" links={studentDatabaseLinks} />
+                <Drawer title="Helpful Links" links={helpfulLinks} />
+                <Drawer title="Jobs" links={jobLinks} />
+                <Drawer title="Internships" links={internshipLinks} />
             </fieldset>
 
 
