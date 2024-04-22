0a421ca477c5f5c07abe0af37933277a8b9067da
Matthew Irizarry <matthewjirizarry@gmail.com>
2024-04-22 13:42:33 -0500

    Updated dashboard container to support a button.

--- components/ui/DashboardContainer.tsx
+++ components/ui/DashboardContainer.tsx
@@ -5,19 +5,24 @@ interface DashboardContainerProps {
     headerText: string
     headerIcon: ReactNode
     children: ReactNode
+    button?: ReactNode
 }
 
 export default function DashboardContainer({
     additionalClasses,
     headerText,
     headerIcon,
-    children
+    children,
+    button
 }: DashboardContainerProps) {
     return (
         <section className={`dashboard-container ${additionalClasses}`}>
             <section className="dashboard-container-header">
                 {headerIcon}
                 <h2>{headerText}</h2>
+                {
+                    button && button
+                }
             </section>
             <section className="dashboard-container-content">
                 {children}
