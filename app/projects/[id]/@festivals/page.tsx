0dc649c7a155ce0d65d8507d3d89dc7dbcad1718
Trevor Kerney <trevorkerney@hotmail.com>
2024-04-22 13:27:07 -0500

    finished project cast & crew

--- app/projects/[id]/@festivals/page.tsx
+++ app/projects/[id]/@festivals/page.tsx
@@ -1,6 +1,7 @@
 import { FaAward } from "react-icons/fa6"
 import DashboardContainer from "~/components/ui/DashboardContainer"
 import prisma from "~/lib/prisma"
+import { dateFromISO8601 } from "~/lib/utils"
 
 interface FestivalsListProps {
   params: {
@@ -20,10 +21,72 @@ export default async function FestivalsList({ params: { id } }: FestivalsListPro
       headerText="Festivals"
       headerIcon={<FaAward />}
     >
-      {festivals.map(({ id, name }) => {
-        return <p key={id}>{name}</p>
-      })}
-
+      <ul className="project-nested-list">
+        {
+          festivals.map(festival => {
+            return (
+              <li key={festival.id} className="project-nested-list-bottom">
+                <p className="project-nested-item-name"><strong>{festival.name}</strong></p>
+                <section className="project-nested-details">
+                  {
+                    (festival.fflink) && (
+                      <a href={festival.fflink}><strong>Film Freeway Link</strong></a>
+                    )
+                  }
+                  {
+                    (festival.email) && (
+                      <p>
+                        <strong>Contact Email: </strong>
+                        { festival.email }
+                      </p>
+                    )
+                  }
+                  {
+                    (festival.strategy) && (
+                      <p>
+                        <strong>Strategy: </strong>
+                        { festival.strategy }
+                      </p>
+                    )
+                  }
+                  {
+                    (festival.status) && (
+                      <p>
+                        <strong>Status: </strong>
+                        { festival.status }
+                      </p>
+                    )
+                  }
+                  {
+                    (festival.earlyDeadline) && (
+                      <p>
+                        <strong>Early Deadline: </strong>
+                        { dateFromISO8601(festival.earlyDeadline.toISOString()) }
+                      </p>
+                    )
+                  }
+                  {
+                    (festival.deadline) && (
+                      <p>
+                        <strong>Deadline: </strong>
+                        { dateFromISO8601(festival.deadline.toISOString()) }
+                      </p>
+                    )
+                  }
+                  {
+                    (festival.submitted) && (
+                      <p>
+                        <strong>Submitted: </strong>
+                        { dateFromISO8601(festival.submitted.toISOString()) }
+                      </p>
+                    )
+                  }
+                </section>
+              </li>
+            )
+          })
+        }
+      </ul>
     </DashboardContainer>
   )
 }
