e02e77b44e5fcccafbafb869929a6f7b7d08d142
Trevor Kerney <trevorkerney@hotmail.com>
2024-04-29 23:29:51 -0500

    removed authentication error throwing

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,5 @@
 "use server"
 
-import { AuthError } from "next-auth"
 import { signIn } from "~/lib/auth"
 import prisma from "~/lib/prisma"
 import { SearchParams } from "./types"
@@ -10,11 +9,7 @@ export async function authenticate(formData: FormData) {
   try {
     await signIn("credentials", formData)
   } catch (error) {
-    if (error instanceof AuthError) {
-      return "We were unable to sign you in. Please contact ITS for assistance with NetID. (270) 745-7000"
-    }
-
-    throw error
+    return "We were unable to sign you in. Please contact ITS for assistance with NetID. (270) 745-7000"
   }
 }
 
